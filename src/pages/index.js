543edece2182182e38f2918b7136a87db548fa49
Rabiu Kabir <reachrabiukabir@gmail.com>
2023-08-14 15:50:03 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from "./AboutPage";
+import Home from "./HomePage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import SingleProduct from "./SingleProductPage";
+import Auth from "./AuthWrapper";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./ProductsPage";
+
+export {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  SingleProduct,
+  Auth,
+  PrivateRoute,
+  Products,
+};
